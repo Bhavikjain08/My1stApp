fa9ec158b38d2a75a251bc7a8f20cc2933efd3b8
Bhavikjain08 <jainbhavik2008@gmail.com>
2023-02-08 00:06:54 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+          child: Center(
+        child: Container(
+          child: Text("Hi I am Baymax..."),
+        ),
+      )),
+    );
+  }
+}
