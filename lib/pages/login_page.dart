196ba191f30db62e3fad793b4107375da7d21409
Bhavikjain08 <jainbhavik2008@gmail.com>
2023-02-11 00:53:20 +0530

    day 8 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
+            child: Form(
+          key: formKey,
           child: Column(
             children: [
+              SizedBox(
+                height: 85.0,
+              ),
               Image.asset(
-                "assets/images/login_image.png",
+                "assets/images/login.png",
                 fit: BoxFit.cover,
               ),
               SizedBox(
                 height: 25.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -32,33 +47,87 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextFormField(
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                       decoration: InputDecoration(
-                        hintText: "Enter UserName",
-                        labelText: "UserName",
+                        hintText: "Enter Username",
+                        labelText: "Username",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
                       ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please Enter Username";
+                        }
+
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 15.0,
                     ),
                     TextFormField(
                       obscureText: true,
                       decoration: InputDecoration(
                         hintText: "Enter Password",
                         labelText: "Password",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
                       ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please Enter Password";
+                        } else if (value.length < 6) {
+                          return "Password Length Too Short... Atleast 6 words needed.";
+                        }
+
+                        return null;
+                      },
                     ),
                     SizedBox(
-                      height: 60.0,
+                      height: 20.0,
                     ),
-                    ElevatedButton(
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                      },
+                    Material(
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 45 : 8),
+                      color: Colors.deepPurple,
+                      child: InkWell(
+                        onTap: () async {
+                          if (formKey.currentState!.validate()) {
+                            setState(() {
+                              changeButton = true;
+                            });
+                            await Future.delayed(Duration(seconds: 3));
+                            await Navigator.pushNamed(
+                                context, MyRoutes.HomeRoute);
+                            setState(() {
+                              changeButton = false;
+                            });
+                          }
+                          ;
+                        },
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 2),
+                          width: changeButton ? 50 : 500,
+                          height: 40,
+                          child: Center(
+                            child: changeButton
+                                ? Icon(Icons.done, color: Colors.white)
+                                : Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white,
+                                  ),
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
               )
             ],
           ),
-        ));
+        )));
   }
 }
