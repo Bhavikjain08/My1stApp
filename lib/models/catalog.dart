922c5609e23701f3b796842d5eecf3714958d881
Bhavikjain08 <jainbhavik2008@gmail.com>
2023-02-12 15:01:59 +0530

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+
+final products = [
+  Item(
+    id: "Product1",
+    name: "Iphone 14 pro",
+    desc: "Apple Iphone 14 pro 256 GB",
+    price: 999,
+    color: "#655d6c",
+    image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-unselect-gallery-2-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1663790290203",
+  )
+];
